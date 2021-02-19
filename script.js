33662f3e8b0927ce25d7a09b5fe21131df96029c
Jon-Morgan <jonmorgan.cobb@gmail.com>
2021-02-18 21:05:43 -0800

    infinite scroll finished

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 const imageContainer = document.getElementById("image-container");
 const loader = document.getElementById("loader");
 
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
 let photosArray = [];
 
 //unsplash API
-const count = 10;
+const count = 30;
 const apiKey = "gJhYUxKlB9y_Zry12fDwe-Mjy07YZDWWhU46A5M8L_0";
 const apiURL = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+//check if all images were loaded
+function imageLoaded() {
+  imagesLoaded++;
+
+  if (imagesLoaded === totalImages) {
+    ready = true;
+    loader.hidden = true;
+  }
+}
 
 //helper function to set attributes on dom elements
 function setAttributes(element, attributes) {
@@ -17,6 +30,10 @@ function setAttributes(element, attributes) {
 
 //create elems for links and photos, add to dom
 function displayPhotos() {
+  imagesLoaded = 0;
+  totalImages = photosArray.length;
+
+  //run function for each object in photosArray
   photosArray.forEach((photo) => {
     //create an <a> to link to unsplash
     const item = document.createElement("a");
@@ -31,6 +48,8 @@ function displayPhotos() {
       alt: photo.alt_description,
       title: photo.alt_description,
     });
+    // event listener, check when each is finished
+    img.addEventListener("load", imageLoaded);
     //put an <img> inside <a>, then put both inside image container element
     item.appendChild(img);
     imageContainer.appendChild(item);
@@ -47,6 +66,17 @@ async function getPhotos() {
     //catch error
   }
 }
+
+// check to see if scrolling near bottom of page, load more photos
+window.addEventListener("scroll", () => {
+  if (
+    window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 &&
+    ready
+  ) {
+    ready = false;
+    getPhotos();
+  }
+});
 
 //on load
 getPhotos();
